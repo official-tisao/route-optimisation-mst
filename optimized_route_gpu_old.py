b978051b92e6af237b80e461b9625eec1e0b1e1a
TIAMIYU SAHEED OLUWATOSIN <38051374+official-tisao@users.noreply.github.com>
2024-10-08 17:35:00 -0400

    Optimize GPU routing logic - chunk 5

--- optimized_route_gpu_old.py
+++ optimized_route_gpu_old.py
@@ -24,3 +24,17 @@ class UnionFindGPU:
     return self.parent[x]
 
 
+  # @cp.fuse()
+  def union(self, x, y):
+    root_x = self.find(x)
+    root_y = self.find(y)
+    if root_x != root_y:
+      if self.rank[root_x] > self.rank[root_y]:
+        self.parent[root_y] = root_x
+      elif self.rank[root_x] < self.rank[root_y]:
+        self.parent[root_x] = root_y
+      else:
+        self.parent[root_y] = root_x
+        self.rank[root_x] += 1
+
+
