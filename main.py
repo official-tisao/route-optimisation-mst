82ebc0d40f45ed72e35c031704f97da088a0fef2
TIAMIYU SAHEED OLUWATOSIN <38051374+official-tisao@users.noreply.github.com>
2024-10-28 15:44:00 -0400

    Implement logic in main.py - chunk 2

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ except Exception:
     gpu_available = False
 
 
+if gpu_available:
+    print("GPU detected. Using GPU optimized script.")
+    from optimized_route_gpu import (boruvka_mst_gpu, kruskal_mst_from_gpu, kruskal_mst_gpu, prim_mst, boruvka_mst)
+                                    # load_data, save_to_csv, build_graph,
+else:
+    print("GPU not detected. Using CPU script.")
+    from optimized_route_open import kruskal_mst, prim_mst, boruvka_mst
+                                     # , load_data, build_graph
+
+
