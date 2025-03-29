61c04935af812d05a7ce1c972087fedb9955c798
Saheed O. Tiamiyu <38051374+official-tisao@users.noreply.github.com>
2025-03-29 16:40:26 -0400

    update

---
+++ union_find.py
@@ -0,0 +1,28 @@
+# File: union_find.py
+class UnionFind:
+    def __init__(self, nodes):
+        self.parent = {node: node for node in nodes}
+        self.rank = {node: 0 for node in nodes}
+
+    def find(self, node):
+        # Iterative approach for path compression
+        root = node
+        while self.parent[root] != root:
+            root = self.parent[root]
+        while node != root:
+            next_node = self.parent[node]
+            self.parent[node] = root
+            node = next_node
+        return root
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
